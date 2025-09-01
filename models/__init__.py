01f1efcf67ae6298ab767bb2aa824a3230c05606
sabrinads23 <131785781+sabrinads23@users.noreply.github.com>
2025-08-31 22:51:20 -0300

    Delete models directory

--- models/__init__.py
+++
@@ -1,9 +0,0 @@
-# Models package - RU 4493981
-from .usuarios import Usuario
-from .pacientes import Paciente
-from .profissionais import Profissional
-from .agendas import Agenda, Horario
-from .consulta import Consulta
-from .prontuario import Prontuario, Receita
-from .administracao import Leito, Internacao, RelatorioFinanceiro, Suprimento
-from .notificacoes import Notificacao
